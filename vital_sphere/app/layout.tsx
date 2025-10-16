880f74c3b73f037372e5af0474bad4d9702dd953
Masato Mizunuma <masatomizunuma911@gmail.com>
2025-10-16 16:48:59 +0800

    Update Commit

--- vital_sphere/app/layout.tsx
+++ vital_sphere/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientProviders from "./context/ClientProviders";
+import './components/swiper_css.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- vital_sphere/app/start/create_account/form.tsx
+++ vital_sphere/app/start/create_account/form.tsx
@@ -3,33 +3,21 @@ import { stringify } from 'querystring';
 import React from 'react'
 import { useState, useContext } from 'react'
 import AuthContext from '@/app/context/AuthContext';
+import SwiperComponent from '@/app/components/SwiperComponent';
 
 const RegistrationForm = () => {
     const { register } = useContext(AuthContext)
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPass] = useState('')
     const [birthday, setBirthday] = useState<Date>(new Date())
     const [isFemale, setFemale] = useState<boolean>(false)
     const [errorMsg, setErrorMsg] = useState('')
 
+
     const onSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
-        if(!username || !password){
-            console.log('Blank forms detected')
-            setErrorMsg('Please fill in the form!')
-            return
-        }
-
-        if(password != confirmPassword){
-            console.log('Passwords don\'t match')
-            setErrorMsg('Passwords don\'t match!')
-            return
-        }
-
-        setErrorMsg('')
 
         try{
             await register(username, password, birthday, isFemale)
@@ -41,39 +29,21 @@ const RegistrationForm = () => {
     }
 
   return (
-    <form onSubmit={onSubmit} className='space-y-8 w-full flex flex-col justify-center item-center'>
-        <div className='flex flex-col items-center'>
-            <h2 className='grid w-full max-w-sm items-center gap-1.5'>User Name</h2>
-            <input 
-                value={username} 
-                onChange={(e) => setUsername(e.target.value)}
-                className='input w-full input-primary input-md font-medium max-w-sm' 
-                type='text' 
-                id='username'/>
-        </div>
-        <div className='flex flex-col items-center'>
-            <h2 className='grid w-full max-w-sm items-center gap-1.5'>Password</h2>
-            <input
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className='input w-full input-primary input-md font-medium max-w-sm' 
-                type="password" 
-                id='password' />
-        </div>
-        <div className='flex flex-col items-center'>
-            <h2 className='grid w-full max-w-sm items-center gap-1.5'>Confirm Password</h2>
-            <input
-                value={confirmPassword}
-                onChange={(e) => setConfirmPass(e.target.value)}
-                className='input w-full input-primary input-md font-medium max-w-sm' 
-                type="password" 
-                id='confirm_password' />
-        </div>
-        <div className='flex flex-col items-center'>
-            <button className='btn btn-primary w-full rounded-lg size-lg max-w-sm mt-5'>Next</button>
-        </div>
-        {errorMsg && <h1 className='text-red-600 font-bold text-center p-2 border border-red-300 bg-red-300 rounded 
-        max-w-sm mx-auto w-full'>{errorMsg}</h1>}
+    <form onSubmit={onSubmit} className='w-full flex flex-col justify-center item-center'>
+        <SwiperComponent
+            username={username}
+            password={password}
+            confirmPassword={confirmPassword}
+            birthday={birthday}
+            isFemale={isFemale}
+            errorMsg={errorMsg}
+            setUsername={setUsername}
+            setPassword={setPassword}
+            setConfirmPass={setConfirmPass}
+            setBirthday={setBirthday}
+            setFemale={setFemale}
+            setErrorMsg={setErrorMsg}
+        />
     </form>
   )
 }
