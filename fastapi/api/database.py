06d7604c0006d51336504705825818d1900fd2ef
Masato Mizunuma <masatomizunuma911@gmail.com>
2026-01-03 16:41:08 +0800

    Settings Implementation v2

--- fastapi/api/database.py
+++ fastapi/api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQL_ALCHEMY_DATABASE_URL = 'postgresql+psycopg://postgres:109789090054HG911@localhost/vital_sphere'
+SQL_ALCHEMY_DATABASE_URL = 'sqlite:///athlete_monitoring.db'
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
